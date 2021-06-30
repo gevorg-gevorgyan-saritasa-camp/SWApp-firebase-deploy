435d74013954b311f7a69921b80bde3bdcbf407b
Gevorg Gevorgyan <gevorg.gevorgyan@saritasa.com>
2021-06-30 17:17:19 +0700

    searching bug fixed

--- js/mainpage/search.js
+++ js/mainpage/search.js
@@ -4,15 +4,14 @@ const search = document.getElementById('search_field');
 
 search.addEventListener('input', () => {
   let body = document.getElementById('films_table');
-  while (body.firstChild) {
-    // This will remove all children within tbody which in your case are <tr> elements
-    body.removeChild(body.firstChild);
-  }
-
   let name = search.value;
 
   db.collection('films').get()
     .then(res => {
+      while (body.firstChild) {
+        // This will remove all children within tbody which in your case are <tr> elements
+        body.removeChild(body.firstChild);
+      }
       res.forEach(doc => {
         if (doc.data().fields.title.includes(name)) {
           createRow(doc.data().fields, doc.data().pk);
