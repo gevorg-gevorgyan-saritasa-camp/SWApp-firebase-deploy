16740ddb564f9c4eebfff7c374b0879853bbbe40
Gevorg Gevorgyan <gevorg.gevorgyan@saritasa.com>
2021-06-30 12:15:34 +0700

    added searching by name, fixed styles

--- js/index.js
+++ js/index.js
@@ -1,22 +1,9 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyAdmnfws0fAtonx9JhFU9-dhfQaZQWY748',
-  authDomain: 'swfilms-eac07.firebaseapp.com',
-  projectId: 'swfilms-eac07',
-  storageBucket: 'swfilms-eac07.appspot.com',
-  messagingSenderId: '584668937233',
-  appId: '1:584668937233:web:70f8c95bd46a1fb24205b0',
-  measurementId: 'G-TK6SH2FDWB',
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
 const table = document.getElementById('films_table');
 
 db.collection('films').get()
   .then(res => {
     res.forEach(doc => {
-      let row = createRow(doc.data().fields, doc.data().pk);
-      table.appendChild(row);
+      createRow(doc.data().fields, doc.data().pk);
     });
   });
 
@@ -40,5 +27,5 @@ function createRow(data, id) {
   row.appendChild(director);
   row.appendChild(releaseDate);
 
-  return row;
+  table.appendChild(row);
 }
