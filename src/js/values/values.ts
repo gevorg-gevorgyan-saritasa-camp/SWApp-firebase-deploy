a0b940810e62f448f41e2e57c3d6a159ba7658b3
Gevorg Gevorgyan <gevorg.gevorgyan@saritasa.com>
2021-07-08 16:17:03 +0700

    fixed buttons styles

--- src/js/values/values.ts
+++ src/js/values/values.ts
@@ -4,10 +4,10 @@ export const DEFAULT_JOIN_ARRAY_SIZE = 10;
 export const DEBOUNCE_DELAY_TIME = 500;
 
 export const HTMLFilmCellsTemplates = {
-  FilmEditCell: '<button class="films-management-button">' +
+  FilmEditCell: '<button title="Edit Film" class="films-management-button">' +
       '<img alt="Edit" src="https://img.icons8.com/ios-glyphs/30/000000/edit--v1.png"/>' +
       '</button>',
-  FilmDeleteCell: '<button class="films-management-button">' +
+  FilmDeleteCell: '<button title="Delete Film" class="films-management-button">' +
       '<img alt="Delete" src="https://img.icons8.com/ios-glyphs/30/000000/delete-forever.png"/>' +
       '</button>',
   MoreInfoCell: '<button class="more-info-button">More info...</button>'
